f29156ea62ac1a935a71c640d2b9761db9c20f52
yaojilong <yaojilong@100tal.com>
2019-11-14 15:35:47 +0800

    add whitinggrid

--- whitinggrid.cpp
+++ whitinggrid.cpp
@@ -1,6 +1,62 @@
 #include "whitinggrid.h"
 
-WhitingGrid::WhitingGrid(QGraphicsItem * parent)
-{
+#include <QPainter>
 
+WhitingGrid::WhitingGrid(QGraphicsObject *parent)
+{
+    WhitingGrid(100,100,WhitingGridType::TinWordFormat,parent);
+}
+
+WhitingGrid::WhitingGrid(int w,int h,WhitingGridType type,QGraphicsObject * parent):m_width(w),m_height(h),type_(type),QGraphicsObject(parent)
+{
+  m_realLineColor = Qt::red;
+  m_dotLineColor = Qt::red;
+  m_dotLineWidth = 3;
+  m_realLineWidth = 3;
+  setFlag(QGraphicsItem::ItemIsFocusable, true);
+}
+
+QRectF WhitingGrid::boundingRect() const
+{
+    return QRectF(0,0,m_width,m_height);
+}
+
+void WhitingGrid::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+        Q_UNUSED(option)
+        Q_UNUSED(widget)
+    switch (getType()) {
+    case WhitingGridType::TinWordFormat:
+        paintTinWordFormat(painter,option,widget);
+        break;
+     case WhitingGridType::FourLinesAndThreeGrids:
+        paintFourLinesAndThreeGrids(painter,option,widget);
+        break;
+    }
+}
+
+void WhitingGrid::paintTinWordFormat(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+        QPen p = QPen(m_realLineColor,m_realLineWidth);
+        painter->setPen(p);
+        QRectF rect(boundingRect());
+        painter->drawRect(rect);
+
+        p.setColor(m_dotLineColor);
+        p.setWidth(m_dotLineWidth);
+        p.setStyle(Qt::DotLine);
+        painter->setPen(p);
+        painter->drawLine(rect.x(),rect.y()+rect.height()/2,rect.right(),rect.y()+rect.height()/2);
+        painter->drawLine(rect.x()+rect.width()/2,rect.y(),rect.x()+rect.width()/2,rect.bottom());
+}
+
+void WhitingGrid::paintFourLinesAndThreeGrids(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+        QPen p = QPen(m_realLineColor,m_realLineWidth);
+        painter->setPen(p);
+        QRectF rect(boundingRect());
+        painter->drawLine(rect.x(),rect.y(),rect.right(),rect.y());
+        painter->drawLine(rect.x(),rect.y()+rect.height()/3,rect.right(),rect.y()+rect.height()/3);
+        painter->drawLine(rect.x(),rect.y()+rect.height()*2/3,rect.right(),rect.y()+rect.height()*2/3);
+        painter->drawLine(rect.x(),rect.bottom(),rect.right(),rect.bottom());
 }
--- whitinggrid.h
+++ whitinggrid.h
@@ -1,13 +1,86 @@
-#ifndef WHITINGGRID_H
+﻿#ifndef WHITINGGRID_H
 #define WHITINGGRID_H
 
 #include <QGraphicsItem>
+#include "TeachingTools_global.h"
 
-class WhitingGrid : public QGraphicsItem
+enum WhitingGridType
+{
+    TinWordFormat, // 田字格
+    FourLinesAndThreeGrids // 四线三格
+};
+class TEACHINGTOOLS_EXPORT WhitingGrid : public QGraphicsObject
 {
     Q_OBJECT
+    Q_PROPERTY(int m_dotLineWidth READ getDotLineWidth WRITE setDotLineWidth)
+    Q_PROPERTY(int m_realLineWidth READ getRealLineWidth WRITE setRealLineWidth)
+    Q_PROPERTY(QColor m_realLineColor READ getRealLineColor WRITE setRealLineColor)
+    Q_PROPERTY(QColor m_dotLineColor READ getDotLineColor WRITE setDotLineColor)
+    Q_PROPERTY(WhitingGridType type_ READ getType WRITE setType)
 public:
-    WhitingGrid(QGraphicsItem * parent = nullptr);
+    WhitingGrid(QGraphicsObject *parent = nullptr);
+    WhitingGrid(int w,int h,WhitingGridType type = WhitingGridType::TinWordFormat,QGraphicsObject *parent = nullptr);
+    QRectF boundingRect()const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+    void paintTinWordFormat(QPainter *painter,const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void paintFourLinesAndThreeGrids(QPainter *painter,const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+    void setDotLineColor(QColor &color){
+
+        this->m_dotLineColor = color;
+        this->update(boundingRect());
+    }
+    void setDotLineWidth(int width){
+        this->m_dotLineWidth =  width;
+        this->update(boundingRect());
+    }
+
+    void setRealLineWidth(int width){
+        this->m_realLineWidth = width;
+        this->update(boundingRect());
+    }
+
+    void setRealLineColor(QColor &color){
+        this->m_realLineColor = color;
+        this->update(boundingRect());
+    }
+
+    int getDotLineWidth() const {
+        return this->m_dotLineWidth;
+    }
+
+    QColor getDotLineColor() const{
+        return this->m_dotLineColor;
+    }
+
+    int getRealLineWidth() const{
+        return this->m_realLineWidth;
+    }
+
+    QColor getRealLineColor() const{
+        return this->m_realLineColor;
+    }
+
+   WhitingGridType getType(){
+        return type_;
+    }
+
+   void setType(WhitingGridType type){
+       this->type_ = type;
+   }
+
+public:
+
+
+private:
+    qreal m_width;
+    qreal m_height;
+    QColor m_dotLineColor;
+    int m_dotLineWidth;
+    QColor m_realLineColor;
+    int m_realLineWidth;
+    WhitingGridType type_;
 };
 
 #endif // WHITINGGRID_H
